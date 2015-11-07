f263c257ecfc17d12317f74cd62b60a9317cacb0
weehowe-z <weehowe.z@gmail.com>
2015-11-07 20:14:03 +0800

    update

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
     var flag = true;
     var clockwise = false;
     var controller = Leap.loop(options, function(frame){
+    if(frame.hands.length > 0) {
+        cardFlip(true, frame.hands[0].type, frame.hands[0].roll());
+    }
+    else {
+        cardFlip(false, "", 0);       // if there are no hands detected, reset the rotations to 0
+      }
     if(frame.valid && frame.gestures.length > 0){
       frame.gestures.forEach(function(gesture){
           switch (gesture.type){
@@ -15,8 +21,6 @@
 
                 /*hack* for getting direction failure*/
                 if (direction === undefined) direction = [0,0,0];
-                //console.log(pointableID);
-                //console.log(direction);
                 /*hack*/
 
                 var dotProduct = Leap.vec3.dot(direction, gesture.normal);//get direction
@@ -48,9 +52,6 @@
                     flag = true;
                 }
                 break;
-            case "keyTap":
-                console.log("Key Tap Gesture");
-                break;
             
             case "screenTap":
                 console.log("Screen Tap Gesture");
@@ -60,21 +61,36 @@
             case "swipe":
                 //Classify swipe as either horizontal or vertical
                 var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+                var duration = gesture.duration;
+                var swipeDirection;
                 //Classify as right-left or up-down
                 if(isHorizontal){
                     if(gesture.direction[0] > 0){
-                        swipeDirection = "right";
+                        if (duration > 5000){
+                          swipeDirection = "right";
+                          makeNormal();
+                        }
                     } else {
-                        swipeDirection = "left";
+                        if (duration > 5000){
+                          swipeDirection = "left";
+                          makeSmall();
+                        }
                     }
                 } else { //vertical
                     if(gesture.direction[1] > 0){
-                        swipeDirection = "up";
+                        //console.log(duration);
+                        if (duration > 5000){
+                          swipeDirection = "up";
+                          unmute();
+                        }
                     } else {
-                        swipeDirection = "down";
+                        if (duration > 5000){
+                          swipeDirection = "down";
+                          mute();
+                        }
                     }                  
                 }
-                console.log(swipeDirection)
+                console.log(swipeDirection);
                 break;
             }
           });
@@ -104,17 +120,58 @@
       myVideo.currentTime -= 10;
     }
 
+    function mute()
+    {
+      myVideo.muted = true;
+    }
+    function unmute()
+    {
+      myVideo.muted = false;
+    }
+
     function makeBig()
     { 
-    myVideo.height=1600; 
+      myVideo.height=1600; 
     } 
 
     function makeSmall()
     { 
-    myVideo.height=320; 
+      myVideo.height=320; 
     } 
 
     function makeNormal()
     { 
-    myVideo.height=640; 
+      myVideo.height=640; 
     } 
+
+
+    function transformCSS (className, degree) {
+      var style = {
+        "-webkit-transform": "rotateY("+degree+"deg)",
+        "-moz-transform": "rotateY("+degree+"deg)",
+        "transform": "rotateY("+degree+"deg)"
+      }
+      $(className).css(style);
+
+      return style;
+    }
+
+
+    function cardFlip(hasHand, handType, rollRadian) {
+      var degree = rollRadian * (180 / Math.PI);
+
+      if(hasHand) {
+          transformCSS(".front", 0-degree);     // front card should roll from 0 to 180
+
+        if(handType === "left")           // hand roll is from 0 to 180deg
+          transformCSS(".back", -degree-180);   // back card should roll from -180 to 0
+        else                    // hand roll is from 0 to -180deg
+          transformCSS(".back", -180-degree);   // back card should roll from -180 to 0
+      }
+      else {
+        transformCSS(".front", 0);
+        transformCSS(".back", -180);
+      }
+    }
+
+
